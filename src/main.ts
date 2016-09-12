104acd4ab21a1e3b7c74f1a2f96ab71e1075b0e0
Liam Ryan <Liam-Ryan@users.noreply.github.com>
2016-09-12 21:15:08 +0100

    Removed Webstorm generated js files

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
 
-console.log(platformBrowserDynamic);
-console.log(enableProdMode);
-console.log(AppModule);
 if (process.env.ENV === 'production') {
     enableProdMode();
 }
