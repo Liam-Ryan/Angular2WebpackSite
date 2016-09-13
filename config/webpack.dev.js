d4656073a087908a54e9926305bf64c67856ac96
Liam Ryan <Liam-Ryan@users.noreply.github.com>
2016-09-13 20:52:40 +0100

    Commited working version

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,7 +3,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
 
-module.exports = webpackMerge( commonConfig, {
+module.exports = webpackMerge(commonConfig, {
   devtool: 'cheap-module-eval-source-map',
 
   output: {
